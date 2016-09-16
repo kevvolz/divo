303735351c5f47de8ede136c6dd30557eb67556b
Kevin Volz <kevvolz@gmail.com>
2016-09-16 11:31:14 -0700

    Create runcardchecker.py
    Splits each row and iterates each item in Runcards.txt (or other) file and searches for errors (57973:13). Outputs boxpoly(s) which stations cannot reach.

---
+++ runcardchecker.py
@@ -0,0 +1,19 @@
+import re
+
+file = raw_input("Enter file name: ")
+if len(file) < 1: file = "Runcards.txt"
+fhand = open(file)
+numlist = list()
+
+print "\nSearching for invalid times...\n"
+
+for line in fhand:
+    pieces = line.split()
+    for i in pieces:
+        if re.search('57973:13', i):    #using regex
+        #if i == "57973:13" or i == '"57973:13' or i == '57973:13"':
+            print "Station cannot reach boxpoly: " + pieces[0]
+    else:
+        continue
+
+print "DONE."
